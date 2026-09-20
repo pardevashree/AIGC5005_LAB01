b283283ec9f11d38fb5419e8e9a95559a7d83a69
pardevashree <n10043242@humber.ca>
2026-09-20 18:32:31 -0400

    added input handling for BMI calculation

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Lab01 environment is working!")
+#Taking input from user; weight in lb and height in feet and inches
+weight_MIN = 95
+weight_MAX = 350
+
+weight = input("enter your weight in pounds(lb): ")
+height_feet = input("enter your height in feet(ft): ")
+height_inches = input("enter your height in inches(in): ")
